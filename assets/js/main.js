9277e6aea2f724c095343572b8a12b0976c6c591
DavidReinon <davidreinongarcia@gmail.com>
2024-01-29 22:22:00 +0100

    fix bug de generar la url api en base al formulari

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,106 +1,111 @@
 // assets/js/main.js
-const defautlUrlApi = "https://opentdb.com/api.php?amount=";
-let allData = null;
+document.addEventListener("DOMContentLoaded", () => {
+    const defautlUrlApi = "https://opentdb.com/api.php?amount=";
+    let allData = null;
 
-//Afegim als botos del HTML les seves respecitves funcions
-document
-    .getElementById("buscarPreguntesApi")
-    .addEventListener("click", () => buscarPreguntesApi());
+    //Afegim als botos del HTML les seves respecitves funcions
+    document
+        .getElementById("buscarPreguntesApi")
+        .addEventListener("click", () => buscarPreguntesApi());
 
-document
-    .getElementById("guardarPreguntesBD")
-    .addEventListener("click", () => guardarPreguntesBD());
+    document
+        .getElementById("guardarPreguntesBD")
+        .addEventListener("click", () => guardarPreguntesBD());
 
-document
-    .getElementById("obtindrePreguntesBD")
-    .addEventListener("click", () => obtindrePreguntesBD());
+    document
+        .getElementById("obtindrePreguntesBD")
+        .addEventListener("click", () => obtindrePreguntesBD());
 
-/**
- * Funció per a obtindre les preguntes de la API
- * @returns {Promise<void>}
- */
-const buscarPreguntesApi = async () => {
-    try {
-        const amount = document.getElementById("trivia_amount").value;
-        let apiUrl = `${defautlUrlApi}${amount}`;
+    /**
+     * Funció per a obtindre les preguntes de la API
+     * @returns {Promise<void>}
+     */
+    const buscarPreguntesApi = async () => {
+        try {
+            const amount = document.getElementById("trivia_amount").value;
+            let apiUrl = `${defautlUrlApi}${amount}`;
 
-        const category = document.getElementById("Categoria").value;
-        if (!category === "any") apiUrl += `&category=${category}`;
+            const category = document.getElementById("trivia_category").value;
+            if (category !== "any") apiUrl += `&category=${category}`;
 
-        const difficulty = document.getElementById("trivia_difficulty").value;
-        if (!difficulty === "any") apiUrl += `&difficulty=${difficulty}`;
+            const difficulty =
+                document.getElementById("trivia_difficulty").value;
+            if (difficulty !== "any") apiUrl += `&difficulty=${difficulty}`;
 
-        const type = document.getElementById("trivia_type").value;
-        if (!type === "any") apiUrl += `&type=${type}`;
+            const type = document.getElementById("trivia_type").value;
+            if (type !== "any") apiUrl += `&type=${type}`;
 
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        allData = data.results;
-        mostrarPreguntes();
-        document.getElementById("guardarPreguntesBD").disabled = false;
-    } catch (error) {
-        console.error("Error al obtindre la informació: " + error);
-        alert("Error al obtindre la informació: " + error);
-    }
-};
-
-/**
- * Funció per a obtindre les preguntes de la BD
- * @returns {Promise<void>}
- */
-const obtindrePreguntesBD = () => {
-    $.ajax({
-        type: "GET",
-        url: "bbdd/get.php",
-        dataType: "json",
-        success: (data) => {
-            allData = data;
+            console.log(apiUrl);
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            allData = data.results;
             mostrarPreguntes();
-            document.getElementById("guardarPreguntesBD").disabled = true;
-        },
-        error: (error) => {
+            document.getElementById("guardarPreguntesBD").disabled = false;
+        } catch (error) {
             console.error("Error al obtindre la informació: " + error);
             alert("Error al obtindre la informació: " + error);
-        },
-    });
-};
+        }
+    };
 
-/**
- * Funció per a guardar les preguntes a la BD
- * @returns {Promise<void>}
- */
-const guardarPreguntesBD = () => {
-    $.ajax({
-        type: "POST",
-        url: "bbdd/post.php",
-        data: JSON.stringify(allData),
-        contentType: "application/json",
-        success: () => {
-            alert("Preguntes guardades correctament.");
-        },
-        error: (error) => {
-            console.error(
-                "Error al guardar preguntes en la base de dades: " + error
-            );
-            alert("Error al guardar preguntes en la base de dades: " + error);
-        },
-    });
-    document.getElementById("guardarPreguntesBD").disabled = true;
-};
+    /**
+     * Funció per a obtindre les preguntes de la BD
+     * @returns {Promise<void>}
+     */
+    const obtindrePreguntesBD = () => {
+        $.ajax({
+            type: "GET",
+            url: "bbdd/get.php",
+            dataType: "json",
+            success: (data) => {
+                allData = data;
+                mostrarPreguntes();
+                document.getElementById("guardarPreguntesBD").disabled = true;
+            },
+            error: (error) => {
+                console.error("Error al obtindre la informació: " + error);
+                alert("Error al obtindre la informació: " + error);
+            },
+        });
+    };
 
-/**
- * Funció per a mostrar les preguntes en el DOM
- * @returns {Promise<void>}
- */
-const mostrarPreguntes = () => {
-    const resultatDiv = document.getElementById("resultadoApi");
-    resultatDiv.innerHTML = "";
+    /**
+     * Funció per a guardar les preguntes a la BD
+     * @returns {Promise<void>}
+     */
+    const guardarPreguntesBD = () => {
+        $.ajax({
+            type: "POST",
+            url: "bbdd/post.php",
+            data: JSON.stringify(allData),
+            contentType: "application/json",
+            success: () => {
+                alert("Preguntes guardades correctament.");
+            },
+            error: (error) => {
+                console.error(
+                    "Error al guardar preguntes en la base de dades: " + error
+                );
+                alert(
+                    "Error al guardar preguntes en la base de dades: " + error
+                );
+            },
+        });
+        document.getElementById("guardarPreguntesBD").disabled = true;
+    };
 
-    allData.forEach((pregunta, index) => {
-        const preguntaDiv = document.createElement("div");
-        preguntaDiv.classList.add("preguntaCompleta");
+    /**
+     * Funció per a mostrar les preguntes en el DOM
+     * @returns {Promise<void>}
+     */
+    const mostrarPreguntes = () => {
+        const resultatDiv = document.getElementById("resultadoApi");
+        resultatDiv.innerHTML = "";
 
-        preguntaDiv.innerHTML = `
+        allData.forEach((pregunta, index) => {
+            const preguntaDiv = document.createElement("div");
+            preguntaDiv.classList.add("preguntaCompleta");
+
+            preguntaDiv.innerHTML = `
             <p class="preguntaString">${index + 1}. ${pregunta.question}</p>
             <div class="respuestas">
                 ${mostrarRespostes(pregunta)}
@@ -108,45 +113,46 @@ const mostrarPreguntes = () => {
             <div class="infoAdicional">${mostrarInfoAdicional(pregunta)}</div>
         `;
 
-        resultatDiv.appendChild(preguntaDiv);
-    });
-};
+            resultatDiv.appendChild(preguntaDiv);
+        });
+    };
 
-/**
- * Funció per a mostrar l'informació adicional de una pregunta
- * @param pregunta
- * @returns {string}
- */
-const mostrarInfoAdicional = (pregunta) => {
-    const infoAdicionalHTML = `<p>Tipus: ${pregunta.type}</p>
+    /**
+     * Funció per a mostrar l'informació adicional de una pregunta
+     * @param pregunta
+     * @returns {string}
+     */
+    const mostrarInfoAdicional = (pregunta) => {
+        const infoAdicionalHTML = `<p>Tipus: ${pregunta.type}</p>
     <p>Dificultat: ${pregunta.difficulty}</p>
     <p>Categoría: ${pregunta.category}</p>`;
 
-    return infoAdicionalHTML;
-};
+        return infoAdicionalHTML;
+    };
 
-/**
- * Funció per a mostrar les respostes amb la correcta resaltada
- * @param pregunta
- * @returns {string}
- */
-const mostrarRespostes = (pregunta) => {
-    let arrayIncorrectAnswers;
+    /**
+     * Funció per a mostrar les respostes amb la correcta resaltada
+     * @param pregunta
+     * @returns {string}
+     */
+    const mostrarRespostes = (pregunta) => {
+        let arrayIncorrectAnswers;
 
-    //En el cas de ser info de la API es un array, si no (desde la BD) es JSON
-    if (Array.isArray(pregunta.incorrect_answers)) {
-        arrayIncorrectAnswers = pregunta.incorrect_answers;
-    } else {
-        arrayIncorrectAnswers = JSON.parse(pregunta.incorrect_answers);
-    }
-    const respostesHTML = arrayIncorrectAnswers.map((incorrect_answer) => {
-        return `<div class="respuesta">${incorrect_answer}</div>`;
-    });
+        //En el cas de ser info de la API es un array, si no (desde la BD) es JSON
+        if (Array.isArray(pregunta.incorrect_answers)) {
+            arrayIncorrectAnswers = pregunta.incorrect_answers;
+        } else {
+            arrayIncorrectAnswers = JSON.parse(pregunta.incorrect_answers);
+        }
+        const respostesHTML = arrayIncorrectAnswers.map((incorrect_answer) => {
+            return `<div class="respuesta">${incorrect_answer}</div>`;
+        });
 
-    // Agregar la resposta correcta amb un estil especial
-    respostesHTML.push(
-        `<div class="respuesta correcta">${pregunta.correct_answer}</div>`
-    );
+        // Agregar la resposta correcta amb un estil especial
+        respostesHTML.push(
+            `<div class="respuesta correcta">${pregunta.correct_answer}</div>`
+        );
 
-    return respostesHTML.join("");
-};
+        return respostesHTML.join("");
+    };
+});
